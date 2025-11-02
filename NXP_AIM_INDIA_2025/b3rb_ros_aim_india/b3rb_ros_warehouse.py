dfb87e75f1335f2162525bdb52d2c7a9dd585901
JATAYU000 <shrivaths44kunju@gmail.com>
2025-11-02 14:23:38 +0530

    v6: f log < 12

--- NXP_AIM_INDIA_2025/b3rb_ros_aim_india/b3rb_ros_warehouse.py
+++ NXP_AIM_INDIA_2025/b3rb_ros_aim_india/b3rb_ros_warehouse.py
@@ -950,7 +950,7 @@ class WarehouseExplore(Node):
 					]
 
 					for ny, nx in neighbors_cardinal:
-						if map_array[ny, nx] == 0 and self.find_obs_around_point((nx,ny),10) <10:  # Free space.
+						if map_array[ny, nx] == 0 and self.find_obs_around_point((nx,ny),10) <12:  # Free space.
 							# self.logger.info(f"obs percent {self.find_obs_around_point((nx,ny),10)}")
 							frontiers.append((ny, nx))
 							break
