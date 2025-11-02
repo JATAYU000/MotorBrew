51601d2cd046712b35f4a68f4ce9a454a5e113d4
JATAYU000 <shrivaths44kunju@gmail.com>
2025-11-02 11:20:57 +0530

    v6: prev commit is c=scan thing btw

--- NXP_AIM_INDIA_2025/b3rb_ros_aim_india/b3rb_ros_warehouse.py
+++ NXP_AIM_INDIA_2025/b3rb_ros_aim_india/b3rb_ros_warehouse.py
@@ -324,7 +324,7 @@ class WarehouseExplore(Node):
 		if self.shelf_info is not None:
 			self.logger.info(f"Map : {self.find_free_space_around_point(self.get_map_coord_from_world_coord(float(self.shelf_info['center'][0]), float(self.shelf_info['center'][1]), self.global_map_curr.info), radius=75)}% free")
 
-		if self.shelf_info is not None and self.find_free_space_around_point(self.get_map_coord_from_world_coord(float(self.shelf_info['center'][0]), float(self.shelf_info['center'][1]), self.global_map_curr.info), radius=75) > 58:
+		if self.shelf_info is not None and self.find_free_space_around_point(self.get_map_coord_from_world_coord(float(self.shelf_info['center'][0]), float(self.shelf_info['center'][1]), self.global_map_curr.info), radius=75) > 48:
 			self.logger.info(f"Map is mostly free, skipping exp: {self.find_free_space_around_point(self.get_map_coord_from_world_coord(float(self.shelf_info['center'][0]), float(self.shelf_info['center'][1]), self.global_map_curr.info), radius=75)}% free")
 			self.current_state = self.MOVE_TO_SHELF
 			return
@@ -1158,7 +1158,7 @@ class WarehouseExplore(Node):
 			self.current_state = self.RECOVER
 		
 		if self.current_state == self.EXPLORE:
-			if self.shelf_info is not None and self.find_free_space_around_point(self.get_map_coord_from_world_coord(float(self.shelf_info['center'][0]), float(self.shelf_info['center'][1]), self.global_map_curr.info), radius=75) > 58:
+			if self.shelf_info is not None and self.find_free_space_around_point(self.get_map_coord_from_world_coord(float(self.shelf_info['center'][0]), float(self.shelf_info['center'][1]), self.global_map_curr.info), radius=75) > 48:
 				self.logger.info(f"CLEAR ENOUGH STAWP FRONTIER")
 				self.cancel_current_goal()
 				self.current_state = self.MOVE_TO_SHELF
