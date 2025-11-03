69119c0b3e87226b719dae9e8ed1290194e039a6
JATAYU000 <shrivaths44kunju@gmail.com>
2025-11-04 01:51:30 +0530

    now explore

--- NXP_AIM_INDIA_2025/b3rb_ros_aim_india/b3rb_ros_warehouse.py
+++ NXP_AIM_INDIA_2025/b3rb_ros_aim_india/b3rb_ros_warehouse.py
@@ -191,7 +191,7 @@ class WarehouseExplore(Node):
 		self.current_shelf_objects = None
 		self.search_point = None
 		self.current_shelf_number = 1
-		self._fb_dist = 36
+		self._fb_dist = 35
 		self._lr_dist = 39
 		self.obj_retry = 0
 
@@ -757,7 +757,7 @@ class WarehouseExplore(Node):
 			info = self.simple_map_curr.info
 			self.logger.info(f"gmap info: {info}")
 			self.logger.info(f"00 : {self.get_map_coord_from_world_coord(0.0,0.0,info)} 11: {self.get_map_coord_from_world_coord(1.0,1.0,info)}")
-			self.current_state = self.DEBUG
+			self.current_state = self.EXLPORE
 
 		elif self.current_state == self.EXPLORE:
 			self.frontier_explore()
@@ -985,7 +985,7 @@ class WarehouseExplore(Node):
 				filtered_message.object_name = filtered_object_names
 				filtered_message.object_count = filtered_object_counts
 				self.current_shelf_objects = filtered_message
-			self.logger.info(f"{filtered_object_names}")
+			# self.logger.info(f"{filtered_object_names}")
 
 
 		# How to send WarehouseShelf messages for evaluation.
