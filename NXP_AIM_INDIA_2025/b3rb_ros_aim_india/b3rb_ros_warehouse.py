8bb7a19db8c6e7cba82b495b6bec58c739aed1fe
JATAYU000 <shrivaths44kunju@gmail.com>
2025-11-03 17:31:25 +0530

    increased distance

--- NXP_AIM_INDIA_2025/b3rb_ros_aim_india/b3rb_ros_warehouse.py
+++ NXP_AIM_INDIA_2025/b3rb_ros_aim_india/b3rb_ros_warehouse.py
@@ -191,8 +191,8 @@ class WarehouseExplore(Node):
 		self.current_shelf_objects = None
 		self.search_point = None
 		self.current_shelf_number = 1
-		self._fb_dist = 40
-		self._lr_dist = 40
+		self._fb_dist = 38
+		self._lr_dist = 38
 		self.obj_retry = 0
 
 		# --- State Machine ---
