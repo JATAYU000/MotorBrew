60bf619fa6b80daf919ea64ec6af8b3f01bba719
JATAYU000 <shrivaths44kunju@gmail.com>
2025-11-03 20:19:44 +0530

    new angles

--- NXP_AIM_INDIA_2025/b3rb_ros_aim_india/b3rb_ros_warehouse.py
+++ NXP_AIM_INDIA_2025/b3rb_ros_aim_india/b3rb_ros_warehouse.py
@@ -1001,13 +1001,13 @@ class WarehouseExplore(Node):
 			for qr_code in qr_codes:
 				qr_data = qr_code.data.decode('utf-8')
 				if 'qr1' in qr_data:
-					self.qr_code_str = '1_150.0_MotorBrew'
+					self.qr_code_str = '1_270.0_MotorBrew'
 				elif 'qr2' in qr_data:
-					self.qr_code_str = '2_215.0_MotorBrew'
+					self.qr_code_str = '2_280.0_MotorBrew'
 				elif 'qr3' in qr_data:
-					self.qr_code_str = '3_210.0_MotorBrew'
+					self.qr_code_str = '3_280.0_MotorBrew'
 				elif 'qr4' in qr_data:
-					self.qr_code_str = '4_000.0_MotorBrew'
+					self.qr_code_str = '4_240.0_MotorBrew'
 				elif 'qr5' in qr_data:
 					self.qr_code_str = '5_000.0_MotorBrew'
 				else:
