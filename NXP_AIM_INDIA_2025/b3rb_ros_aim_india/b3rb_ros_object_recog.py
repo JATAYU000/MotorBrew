1a3f08340a08a844953832cb74ebe5e3caf85952
Drone944 <manasvarma944@gmail.com>
2025-11-04 11:24:05 +0530

    yolov8m_elq

--- NXP_AIM_INDIA_2025/b3rb_ros_aim_india/b3rb_ros_object_recog.py
+++ NXP_AIM_INDIA_2025/b3rb_ros_aim_india/b3rb_ros_object_recog.py
@@ -223,7 +223,9 @@ class ObjectRecognizer(Node):
         resource_path_coco = pkg_resources.resource_filename(
             PACKAGE_NAME, resource_name_coco
         )
-        resource_name_yolo = "../../../../share/ament_index/resource_index/yolov8m_full_integer_quant.tflite"
+        resource_name_yolo = (
+            "../../../../share/ament_index/resource_index/yolov8m_elq.tflite"
+        )
         resource_path_yolo = pkg_resources.resource_filename(
             PACKAGE_NAME, resource_name_yolo
         )
--- NXP_AIM_INDIA_2025/setup.py
+++ NXP_AIM_INDIA_2025/setup.py
@@ -9,13 +9,9 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/ament_index/resource_index", ["resource/coco.yaml"]),
-        # ("share/ament_index/resource_index", ["resource/yolov5n-int8.tflite"]),
-        # ("share/ament_index/resource_index", ["resource/yolo11n_int8.tflite"]),
-        # ("share/ament_index/resource_index", ["resource/yolov8m_int8.tflite"]),
-        # ("share/ament_index/resource_index", ["resource/yolo11m_integer_quant.tflite"]),
         (
             "share/ament_index/resource_index",
-            ["resource/yolov8m_full_integer_quant.tflite"],
+            ["resource/yolov8m_elq.tflite"],
         ),
         ("share/" + package_name, ["package.xml"]),
     ],
