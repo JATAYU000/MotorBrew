2d6e58163307ec1575ea84f0be3e341146164cfd
JATAYU000 <shrivaths44kunju@gmail.com>
2025-11-05 10:19:11 +0530

    Final testing remains shelf info

--- NXP_AIM_INDIA_2025/b3rb_ros_aim_india/b3rb_ros_warehouse.py
+++ NXP_AIM_INDIA_2025/b3rb_ros_aim_india/b3rb_ros_warehouse.py
@@ -793,6 +793,7 @@ class WarehouseExplore(Node):
 			self.logger.info(f"gmap info: {info}")
 			self.logger.info(f"00 : {self.get_map_coord_from_world_coord(0.0,0.0,info)} 11: {self.get_map_coord_from_world_coord(1.0,1.0,info)}")
 			self.current_state = self.WAIT_FRONTIER
+			self.shelf_info = None
 		
 		elif self.current_state == self.WAIT_FRONTIER:
 			self.frontier_explore_while_detect()
