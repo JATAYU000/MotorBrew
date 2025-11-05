97d4f8ddaf1738af4086b106e2eb621a798b0bb6
JATAYU000 <shrivaths44kunju@gmail.com>
2025-11-05 10:50:20 +0530

    Final Explore code Hopefully

--- NXP_AIM_INDIA_2025/b3rb_ros_aim_india/b3rb_ros_warehouse.py
+++ NXP_AIM_INDIA_2025/b3rb_ros_aim_india/b3rb_ros_warehouse.py
@@ -189,7 +189,7 @@ class WarehouseExplore(Node):
 		self.current_shelf_objects = None
 		self.search_point = None
 		self.current_shelf_number = 1
-		self._fb_dist = 30
+		self._fb_dist = 28
 		self._lr_dist = 39
 		self.obj_retry = 0
 
@@ -244,7 +244,7 @@ class WarehouseExplore(Node):
 
 	def handle_move_to_shelf(self):
 		self.front, self.back = self.find_front_back_points(self._fb_dist,False)
-		self.f, self.b = self.find_front_back_points(30,False)
+		self.f, self.b = self.find_front_back_points(28,False)
 
 		direction = self.shelf_info['orientation']['secondary_direction']
 		self.logger.info(f" f b : {self.find_obs_around_point(self.front, 30)}, {self.find_obs_around_point(self.back,30)}")
@@ -269,7 +269,7 @@ class WarehouseExplore(Node):
 			self.shelf_objects_curr.object_count = self.current_shelf_objects.object_count
 			self.logger.info(f"Captured {self.shelf_objects_curr.object_count} objects: {self.shelf_objects_curr.object_name}")
 
-			if sum(self.current_shelf_objects.object_count) >=2 or self.obj_retry>5:
+			if sum(self.current_shelf_objects.object_count) ==6 or self.obj_retry>5:
 				info = self.find_obstacles_on_ray()
 				if info is not None: self.shelf_info = info
 				self.current_state = self.MOVE_TO_QR
@@ -278,7 +278,7 @@ class WarehouseExplore(Node):
 				if info is not None: self.shelf_info = info
 				self.logger.info("Adjusting position to capture all objects...")
 				if self._fb_dist < 27: self._fb_dist = 28
-				else: self._fb_dist == 25
+				else: self._fb_dist == 23
 				self.obj_retry +=1
 				self.current_state = self.MOVE_TO_SHELF
 		else:
